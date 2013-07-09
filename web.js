635090f5136d2439ec7a34b7090f8436f2f193a4
unknown <semion44@gmail.com>
2013-07-08 18:33:30 -0700

    web.js require('buffer');

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var fs = require('fs');
+var buf = require('buffer');
 
 var html = fs.readFileSync('./index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(Buffer.toString(html));
+  response.send(buf.toString(html));
 });
 
 var port = process.env.PORT || 5000;
