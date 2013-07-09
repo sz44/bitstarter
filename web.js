3b95445e6bdcd064ffa28e0a1cb6c66e537fc467
unknown <semion44@gmail.com>
2013-07-08 18:18:57 -0700

    fs and buffer for web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+
+var html = fs.readFileSync('./index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(Buffer.toString(html));
 });
 
 var port = process.env.PORT || 5000;
