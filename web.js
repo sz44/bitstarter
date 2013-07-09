1c5e032e1ab365b6a37bbbd0e4b56aeb9ef3691d
unknown <semion44@gmail.com>
2013-07-08 19:09:46 -0700

    html.toString

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var html = fs.readFileSync('./index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(html));
+  response.send(html.toString());
 });
 
 var port = process.env.PORT || 5000;
